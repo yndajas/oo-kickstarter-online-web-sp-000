9dd3dcf0a79744d969f3cf5004c99e8101e4b467
Ynda Jas <yndajas@gmail.com>
2020-07-22 02:18:58 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,7 +9,7 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    backer.back_project(self) if !backer.projects.include?(project)
+    backer.back_project(self) if !backer.projects.include?(self)
   end
   
 end
